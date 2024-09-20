5a05c3ffd73a9d6bd4d883765961254783542dc7
hadxr <hadikhan05@hotmail.co.uk>
2024-09-20 20:14:25 +0100

    Big Changes

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import circleshape
 import pygame
+from constants import *
+import random
 
 class Asteroid(circleshape.CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,23 @@ class Asteroid(circleshape.CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20,50)
+
+        angle1 = self.velocity.rotate(random_angle)
+        angle2 = self.velocity.rotate(-random_angle)
+
+        newRadius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroidSmall1 = Asteroid(self.position.x,self.position.y,newRadius)
+        asteroidSmall2 = Asteroid(self.position.x,self.position.y,newRadius)
+
+        asteroidSmall1.velocity = angle1 * 1.2
+        asteroidSmall2.velocity = angle2 * 1.2
         
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from circleshape import CircleShape
+from shot import Shot
 
 
 
@@ -15,9 +17,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable,drawable)
     Asteroid.containers = (asteroids,updatable,drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     asteroidField = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
@@ -29,6 +33,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for item in asteroids:
+            gameStatus = item.check_collision(player)
+            if gameStatus:
+                print("Game over!")
+                pygame.quit()
+                exit()
+            for bullet in shots:
+                asteroidKill = item.check_collision(bullet)
+                if asteroidKill == True:
+                    item.split()
+                    bullet.kill()
+
 
         for item in updatable:
             item.update(dt)
